1c3d82380e5e72e6e8a009fc1c6954fd6022b46e
Jake Backer <jbacker42@gmail.com>
2017-09-30 14:26:52 -0400

    Base vision code

---
+++ src/org/robockets/robot/ImageProcessor.java
@@ -0,0 +1,12 @@
+package org.robockets.robot;
+
+import edu.wpi.first.wpilibj.vision.VisionPipeline;
+import org.opencv.core.Mat;
+
+public class ImageProcessor implements VisionPipeline {
+
+	@Override
+	public void process(Mat image) {
+
+	}
+}
---
+++ src/org/robockets/robot/vision/VisionManager.java
@@ -0,0 +1,14 @@
+package org.robockets.robot.vision;
+
+import edu.wpi.first.wpilibj.vision.VisionRunner;
+import org.robockets.robot.ImageProcessor;
+
+public class VisionManager implements VisionRunner.Listener<ImageProcessor>{
+
+	
+
+	@Override
+	public void copyPipelineOutputs(ImageProcessor pipeline) {
+
+	}
+}
