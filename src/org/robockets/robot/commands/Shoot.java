06986a37cd1311bacc048aef3135838d5b3d47ec
Michael Capone <michaelcapone21@gmail.com>
2017-12-12 19:41:31 -0500

    Finish shooter code and add button

---
+++ src/org/robockets/robot/commands/Shoot.java
@@ -0,0 +1,16 @@
+package org.robockets.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+
+/**
+ *
+ */
+public class Shoot extends CommandGroup {
+
+    public Shoot() {
+    	addParallel(new SpinSpinners());
+    	addSequential(new WaitCommand(2));
+    	addParallel(new Fire());
+    }
+}
